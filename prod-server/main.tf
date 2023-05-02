6f7746759d70088874d1a731840e9fc59231fab0
Srija1991 <reachsrija@gmail.com>
2023-05-02 13:03:53 +0530

    Prodserver

---
+++ prod-server/main.tf
@@ -0,0 +1,24 @@
+  resource "aws_instance" "prod-server" {
+  ami           = "ami-02eb7a4783e7e9317" 
+  instance_type = "t2.medium" 
+  key_name = "apr26"
+  vpc_security_group_ids= ["sg-0c69f259b0ea97dc0"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./apr26.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "prod-server"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.prod-server.public_ip} > inventory "
+  }
+   provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/prod-server/k8playbook.yml "
+  } 
+}
